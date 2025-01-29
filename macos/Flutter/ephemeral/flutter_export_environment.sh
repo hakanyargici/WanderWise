860d3248775989f59afb386a7b1764b749d87975
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2025-01-29 20:05:47 +0300

    Bottom menu oluşturuldu ve dizaynı yapıldı.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/rabiyatanbas/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rabiyatanbas/Desktop/mobilProgramlama"
+export "FLUTTER_ROOT=/Users/hakanyargici/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hakanyargici/Desktop/WanderWise"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.1.0"
