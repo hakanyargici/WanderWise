07a3af43006eeb5440d7accc5eb32cf4cd8c5777
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2025-01-30 22:28:25 +0300

    Trip Screen tamamlandı.

---
+++ lib/screens/trip_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../core/constants.dart';
+import '../core/themes.dart';
+import '../widgets/bottom_menu.dart';
+
+class TripScreen extends StatelessWidget {
+  const TripScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            title: Text(
+              "Seyahat Rehberi",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                childAspectRatio: 0.85,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 16 / 9,
+                        child: Container(
+                          color:
+                              Theme.of(context).colorScheme.secondaryContainer,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Başlık ${index + 1}",
+                              style: Theme.of(context).textTheme.titleMedium,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              "Alt başlık açıklama yazısı burada yer alır",
+                              style: Theme.of(context).textTheme.bodySmall,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                childCount: 10,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomMenu(),
+    );
+  }
+}
