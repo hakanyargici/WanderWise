0483b1310406c27fe406bbffa8420cfed75d25fb
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2025-01-30 13:23:52 +0300

    2 sayfa eklendi.

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import '../screens/boarding_screen.dart';
+import '../screens/home_screen.dart';
+
+// Router yapılandırması
+final router = GoRouter(
+  initialLocation: '/', // Başlangıç rotası
+  routes: [
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const LoadingScreen(),
+      ),
+    ),
+    GoRoute(
+      path: '/home',
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const HomeScreen(),
+      ),
+    ),
+  ],
+);
